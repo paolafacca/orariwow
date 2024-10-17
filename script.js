5c67c773aa7756adc742c35d8a5a6b2b6ae3eb8f
paolafacca <149096806+paolafacca@users.noreply.github.com>
2024-10-17 10:53:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function getReturnTime(dayOfWeek) {
     const returnTimes = {
         'lunedì': '19:45',
         'martedì': '18:45',
-      //  'mercoledì': '18:45',
-        'giovedì': '18:45',
+        'mercoledì': '18:45',
+        'giovedì': '19:45',
         'venerdì': '18:45',
         'sabato': 'xx:xx',
         'domenica': 'xx:xx'
